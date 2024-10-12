338e08b9a92487819c7f2f62b5332cf5626ccb8d
renatakov <renatakov1010@gmail.com>
2024-10-12 21:39:11 +0200

    1

---
+++ task-1/routes/pl.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', async (req, res, next)=>{
+    const [data] = await global.conn.execute('SELECT * FROM programming languages');
+    res.render('pl', {"data": data});
+})
+
+module.exports = router;
