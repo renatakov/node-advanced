06a8fbde4854f0de44fe3aae5493f7b2e83cf9bf
renatakov <renatakov1010@gmail.com>
2024-11-02 18:42:26 +0100

    1

--- task-3/routes/test.js
+++ task-3/routes/test.js
@@ -3,7 +3,9 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('test', { title: 'Express' });
+    const q = req.query;
+
+    res.json(q);
 });
 
 module.exports = router;
