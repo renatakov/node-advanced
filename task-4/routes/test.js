199d47e460ef8619ec77d18d9e07d8c1b30132dc
renatakov <renatakov1010@gmail.com>
2024-11-02 19:00:40 +0100

    1

--- task-4/routes/test.js
+++ task-4/routes/test.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post('/', function (req, res, next) {
+    const q = req.body;
+
+    res.json(q);
 });
 
 module.exports = router;
