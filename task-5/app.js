3f9306b0e9b7499e0ee273555cec0d115603910d
renatakov <renatakov1010@gmail.com>
2024-11-15 21:01:52 +0100

    1

--- task-5/app.js
+++ task-5/app.js
@@ -2,12 +2,13 @@ const express = require('express');
 const multer = require('multer');
 const port = 3000;
 const path = require('path');
-
+const CONFIG = require('./db/config');
+const mysql = require('mysql2/promise');
 
 const app = express();
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb)=>{
+    destination: (req, file, cb) => {
         cb(null, 'files/')
     },
     filename: (req, file, cb) => {
@@ -18,25 +19,32 @@ const storage = multer.diskStorage({
 
 const fileFilter = (req, file, cb) => {
     const fileLength = parseInt(req.headers['content-length'])
-    const fileTypes = ['application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document']
-    if(fileTypes.includes(file.mimetype) && fileLength < 1000){
+    const fileTypes = ["application/msword", "application/vnd.openxmlformats-officedocument.wordprocessingml.document"]
+    if (fileTypes.includes(file.mimetype) && fileLength < 1000000) {
         cb(null, true)
     } else {
         cb(null, false)
     }
 }
 
-const upload = multer({storage: storage, fileFilter: fileFilter});
+const upload = multer({ storage: storage, fileFilter: fileFilter });
 
 app.get('/', (req, res) => {
     res.json({ "work": true })
 });
 
-app.get('/task-5', (req, res)=>{
-    res.send('<form method="POST" action="/task-5" enctype="multipart/form-data"><input type="file" id="file" name="file"><input type="submit" value="go"/></form>')
+
+app.use(async (req, res, next) => {
+    global.conn = await mysql.createPool(CONFIG);
+    next()
 })
 
-app.post('/task-5', upload.single('file'), (req, res)=>{
+app.get('/task-5', (req, res) => {
+    res.send('<form method="POST" action="/task-5" enctype="multipart/form-data"><input type="file"/id="file" name="file"><input type="submit" value="go"/></form>')
+})
+
+app.post('/task-5', upload.single('file'), async (req, res) => {
+
     res.json('file uploaded successfully')
 })
 
---
+++ task-5/db/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    host: "",
+    user: "root@localhost",
+    password: "",
+    database: "express_task5",
+    port: "3306",
+    // ===== pool
+    waitForConnections: true,
+    connectionLimit: 10,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 0,
+}
